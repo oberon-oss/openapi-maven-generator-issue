86b716c7a87e659fa0a3aadcb1b5a70fe4dceee8
Fabien H. Dumay <fabien.h.dumay@oberon-oss.org>
2024-06-17 00:56:58 +0200

    Added issue information

--- src/main/java/org/example/openapi/mvn/ObjectController.java
+++ src/main/java/org/example/openapi/mvn/ObjectController.java
@@ -1,35 +1,28 @@
 package org.example.openapi.mvn;
 
-import org.example.openapi.mvn.generated.api.ObjectApi;
 import org.example.openapi.mvn.generated.api.ObjectsApi;
-import org.example.openapi.mvn.generated.dto.GetObject200Response;
-import org.example.openapi.mvn.generated.dto.ObjectsGet200ResponseInner;
+import org.example.openapi.mvn.generated.dto.GetAllObjects200ResponseInner;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.NativeWebRequest;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-public class ObjectController implements ObjectsApi, ObjectApi {
+@RequestMapping("/v1")
+public class ObjectController implements ObjectsApi {
     @Override
-    public Optional<NativeWebRequest> getRequest() {
-        return ObjectsApi.super.getRequest();
+    public ResponseEntity<List<GetAllObjects200ResponseInner>> getAllObjects() {
+        return null;
     }
 
     @Override
-    public ResponseEntity<List<ObjectsGet200ResponseInner>> objectsGet() {
-        return ObjectsApi.super.objectsGet();
+    public ResponseEntity<GetAllObjects200ResponseInner> getObject() {
+        return null;
     }
 
     @Override
-    public ResponseEntity<GetObject200Response> getObject() {
-        return ObjectApi.super.getObject();
-    }
-
-    @Override
-    public ResponseEntity<GetObject200Response> postObject(GetObject200Response getObject200Response) {
-        return ObjectApi.super.postObject(getObject200Response);
+    public ResponseEntity<GetAllObjects200ResponseInner> postObject(GetAllObjects200ResponseInner getAllObjects200ResponseInner) {
+        return null;
     }
 }
